e903c7f97102ab2f9f60d6865c9bfda09256c7ab
duantihua <duantihua@gmail.com>
2012-11-22 11:05:52 +0800

    new Ems models

---
+++ org.beangle.ems.core/src/main/java/org/beangle/ems/web/tags/EmsModels.java
@@ -0,0 +1,89 @@
+/*
+ * Beangle, Agile Java/Scala Development Scaffold and Toolkit
+ *
+ * Copyright (c) 2005-2012, Beangle Software.
+ *
+ * Beangle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Beangle is distributed in the hope that it will be useful.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Beangle.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.beangle.ems.web.tags;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.beangle.ems.web.tags.component.AvatarImage;
+import org.beangle.ems.web.tags.component.Code;
+import org.beangle.ems.web.tags.component.Guard;
+import org.beangle.ems.web.tags.component.Userinfo;
+import org.beangle.security.access.AuthorityManager;
+import org.beangle.struts2.view.component.Component;
+import org.beangle.struts2.view.freemarker.AbstractModels;
+import org.beangle.struts2.view.freemarker.TagModel;
+
+import com.opensymphony.xwork2.util.ValueStack;
+
+/**
+ * @author chaostone
+ * @since 3.0.2
+ */
+public class EmsModels extends AbstractModels {
+
+  AuthorityManager authorityManager;
+
+  public EmsModels(ValueStack stack, HttpServletRequest req, HttpServletResponse res) {
+    super(stack, req, res);
+  }
+
+  public TagModel getGuard() {
+    TagModel model = models.get(Guard.class);
+    if (null == model) {
+      model = new TagModel(stack) {
+        protected Component getBean() {
+          return new Guard(stack, authorityManager);
+        }
+      };
+      models.put(Guard.class, model);
+    }
+    return model;
+  }
+
+  public TagModel getAvatar() {
+    TagModel model = models.get(AvatarImage.class);
+    if (null == model) {
+      model = new TagModel(stack) {
+        protected Component getBean() {
+          return new AvatarImage(stack, authorityManager);
+        }
+      };
+      models.put(AvatarImage.class, model);
+    }
+    return model;
+  }
+
+  public TagModel getUserinfo() {
+    TagModel model = models.get(Userinfo.class);
+    if (null == model) {
+      model = new TagModel(stack) {
+        protected Component getBean() {
+          return new Userinfo(stack, authorityManager);
+        }
+      };
+      models.put(Userinfo.class, model);
+    }
+    return model;
+  }
+
+  public TagModel getCode() {
+    return get(Code.class);
+  }
+}
